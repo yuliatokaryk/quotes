0dcecc2f06d2a1f366dc4257388f70e2a4844ae9
yuliatokaryk <julia4099@gmail.com>
2022-07-30 12:36:51 +0300

    added authors table

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,20 @@
+class AuthorsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
