85c06643aa8792b8828097423e505bb35097d178
yuliatokaryk <julia4099@gmail.com>
2022-07-30 13:03:04 +0300

    crud added

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,20 +1,42 @@
 class AuthorsController < ApplicationController
+
   def show
+    @author = Author.find(params[:id])
   end
 
   def index
+    @authors = Author.all
   end
 
-  def edit
-
+  def new
+    @author = Author.new
   end
 
   def create
+    @author = Author.new(params.require(:author).permit(:first_name, :socond_name, :date_of_birth, :wikipedia_url, :goodreads_url))
+    @author.user = current_user
+    if @author.save
+      flash[:notice] = "Author was saved"
+      redirect_to authors_path
+    else
+      render 'new'
+    end
+  end
+
+
+  def edit
   end
 
   def update
+    if @author.update(params.require(:author).permit(:first_name, :socond_name, :date_of_birth, :wikipedia_url, :goodreads_url))
+      flash[:notice] = "Author was updated"
+      redirect_to @author
+    else
+      render 'edit'
+    end
   end
 
   def destroy
   end
+
 end
