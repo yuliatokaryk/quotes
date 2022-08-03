c00b0416fbab1ccc787a603b6c0d9db651a88c9b
yuliatokaryk <julia4099@gmail.com>
2022-08-03 13:41:23 +0300

    books fixed

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,5 +6,4 @@ class Author < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
-
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Book, :type => :model do
       expect(subject).to be_valid
     end
 
-    it "is not valid without a title" do
+    it 'is not valid without a title' do
       subject.title = nil
       expect(subject).to_not be_valid
     end
