43073d7aff4ec5afc68dcd39548e23b7dff66ed0
yuliatokaryk <julia4099@gmail.com>
2022-08-22 16:23:02 +0300

    profile fixed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,12 +14,10 @@ class ProfilesController < ApplicationController
   def create
     @profile = Profile.new(profile_params)
     @profile.user = current_user
-
     if @profile.save
-      flash[:alert] = "Profile was saved"
-      redirect_to root_path
+      flash[:notice] = "Profile was saved"
+      redirect_to profile_path
     else
-      flash[:alert] = "Profile wasn't saved"
       render 'new'
     end
   end
