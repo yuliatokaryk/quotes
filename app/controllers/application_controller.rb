2fae95eec5859f59f992b92c8e0989144d06f9fe
yuliatokaryk <julia4099@gmail.com>
2023-02-19 19:57:34 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action do
-    I18n.locale = :uk
-  end
   before_action :authenticate_user!
 
   include Pagy::Backend
   include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
- 
+
   private
- 
+
     def user_not_authorized
       flash[:warning] = "You are not authorized to perform this action."
       redirect_to(request.referrer || root_path)
