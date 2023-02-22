e3682a5f8af3f7e77df4f1669da49481a1e4f4b6
yuliatokaryk <julia4099@gmail.com>
2023-02-22 17:09:41 +0200

    styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,18 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def after_sign_up_path_for(resource)
+    if resource.is_a?(User)
+      books_path
+    else
+      super
+    end
+  end
+
   private
 
-    def user_not_authorized
-      flash[:warning] = "You are not authorized to perform this action."
-      redirect_to(request.referrer || root_path)
-    end
+  def user_not_authorized
+    flash[:warning] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
