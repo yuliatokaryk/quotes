73919e5ca9ef60a3a5f363bc7f2cec519695e330
yuliatokaryk <julia4099@gmail.com>
2023-03-02 19:09:44 +0200

    followers and following

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,12 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :avatar
 
+  has_many :followers, through: :followers, source: :follower
+  has_many :following, through: :following, source: :following
+
+  has_many :follower_relationships, class_name: 'Follower', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following_relationships, class_name: 'Following', foreign_key: 'following_id', dependent: :destroy
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :nickname, presence: true, uniqueness: true
