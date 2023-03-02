29733630bbb92ead68dafc7e3e6eca37ee94e435
yuliatokaryk <julia4099@gmail.com>
2023-03-02 19:41:43 +0200

    followers and following models

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,2 +1,4 @@
 class Follower < ApplicationRecord
+  belongs_to :profile
+  belongs_to :follower, class_name: 'Profile', foreign_key: 'follower_id'
 end
--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,4 @@
 class Following < ApplicationRecord
+  belongs_to :profile
+  belongs_to :following, class_name: 'Profile', foreign_key: 'follower_id'
 end
