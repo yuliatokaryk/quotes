2762445f958ba2728227ce609e39c27ae809147b
yuliatokaryk <julia4099@gmail.com>
2023-03-04 18:25:08 +0200

    added stimulus

---
+++ app/javascript/controllers/index.js
@@ -0,0 +1,9 @@
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context(".", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+import NavbarController from "./navbar_controller"
+application.register("navbar", NavbarController)
